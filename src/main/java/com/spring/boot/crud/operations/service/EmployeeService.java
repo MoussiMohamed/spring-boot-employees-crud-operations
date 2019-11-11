cff4e3b3ab5eb07b256392381e68bfb1b1bbc74e
MoussiMohamed <mohamedmo@xtech.guru>
2019-11-11 22:08:25 +0100

    Implement crud employee

---
+++ src/main/java/com/spring/boot/crud/operations/service/EmployeeService.java
@@ -0,0 +1,4 @@
+package com.spring.boot.crud.operations.service;
+
+public interface EmployeeService {
+}
---
+++ src/main/java/com/spring/boot/crud/operations/service/EmployeeServiceImpl.java
@@ -0,0 +1,4 @@
+package com.spring.boot.crud.operations.service;
+
+public class EmployeeServiceImpl {
+}
